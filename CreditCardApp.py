0b87bfa8f6e54e7255365c2eb56af2e4c9831672
victorcv56 <victorcv56@gmail.com>
2023-11-16 15:40:43 -0800

    use writer.py module to experiment with writing data to file.

--- CreditCardApp.py
+++ CreditCardApp.py
@@ -1,10 +1,36 @@
-from newBank import bankCards as creditCard
+from newBank import bankCards as cc
+from writer import logger as l
 
-barclays = creditCard("Barclays", 29.99, 1511.65)
+barclays = cc("Barclays", 29.99, 1511.65)
 print(barclays)
-wells = creditCard("Wells Fargo", 19.24, 9942.73)
+wells = cc("Wells Fargo", 19.24, 10141.16)
 print(wells)
+amazon = cc("Amazon", 27.49, 6748.22)
+print(amazon)
 
-wells_interest = wells.get_monthly_interest()
-print("{:.2f}".format(wells_interest))
-wells.pay_off_in_given_time()
+print("")
+wells_interest = wells.get_total_interest()
+print(wells_interest)
+
+barclays_interest = barclays.get_total_interest()
+print(barclays_interest)
+
+amazon_interest = amazon.get_total_interest()
+print(amazon_interest)
+
+
+print("")
+prompt = input("Would you like to create a new file(y/n):\n ")
+if prompt == 'y':
+    filename = input("Please enter name of file: ")
+    fo = l(filename)
+    fo.create_new_file()
+else:
+    filename = input("Please enter name of existing file:\n ")
+    fo = l(filename)
+
+fo.add_to_file(wells_interest)
+fo.add_to_file(amazon_interest)
+fo.add_to_file(barclays_interest)
+fo.write_to_file(barclays_interest) # write_to_file method will overwrite all data...
+fo.read_from_file()
