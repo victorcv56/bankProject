a9ef9646338767819b190e860a305e171847b743
victorcv56 <victorcv56@gmail.com>
2023-12-06 16:18:47 -0800

    work with dictionaries inside main

--- newbank/CreditCardApp.py
+++ newbank/CreditCardApp.py
@@ -14,7 +14,14 @@ amazon = cc("Amazon", 27.49, 6748.22)
 print(amazon)
 # card_list.append(amazon) # add card object to list
 
+card_dict = {}
+card_name = amazon.name
+card_apr = amazon.apr
+card_bal = amazon.bal
+card_obj = {card_name: {'apr': card_apr, 'balance': card_bal}}
+card_dict.update(card_obj)
 
+print(card_dict)
 # card_dict = li() # initializing dictionary
 # print("type of card_dict is: ", end='')
 # print(type(card_dict))
@@ -22,6 +29,8 @@ print(amazon)
 
 
 print("")
+# Calculates card's interest using class method, 
+# then displays it for user
 wells_interest = "{} card total interest: ${:.2f}".format(wells.name, wells.get_total_interest())
 print(wells_interest)
 
@@ -31,10 +40,13 @@ print(barclays_interest)
 amazon_interest = "{} card total interest: ${:.2f}".format(amazon.name, amazon.get_total_interest())
 print(amazon_interest)
 
-amazon.write_data()
-barclays.write_data()
-wells.write_data()
+amazon.write_data() # writes data to .txt file
+barclays.write_data() # writes data to .txt file
+wells.write_data() # writes data to .txt file
 
 amazon_apr = "{}'s APR: {}%".format(amazon.name, amazon.apr)
 print(amazon_apr)
 
+# Add time methods? learn python time lib to be able to assimilate to 
+# an actual app that keeps track of time to be able to use missed payment
+# and late fees. 
